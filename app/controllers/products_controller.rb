78a4c424ace783d5bb3bbc3dc9cc0f5886c786b9
iantheninja <munene.ian@live.com>
2015-05-10 11:54:51 +0300

    index page formated, edit working for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,6 +13,15 @@ include ProductsHelper
 
 		redirect_to product_path(@product)
 	end
+	def edit 
+		@product = Product.find(params[:id])
+	end
+	def update 
+		@product = Product.find(params[:id])
+		@product.update(product_params)
+
+		redirect_to product_path(@product)
+	end
 	def destroy 
 		@product = Product.find(params[:id])
 		@product.destroy
